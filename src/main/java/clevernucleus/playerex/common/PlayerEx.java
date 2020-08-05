96dfaf8c0b00320bd6924534cc35453698add8e4
CleverNucleus <jamie@mclean007.plus.com>
2020-08-05 20:55:58 +0100

    Created Main mod file
    +PlayerEx

---
+++ src/main/java/clevernucleus/playerex/common/PlayerEx.java
@@ -0,0 +1,15 @@
+package clevernucleus.playerex.common;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Mod init class; holds the modid.
+ */
+@Mod(PlayerEx.MODID)
+public class PlayerEx {
+	
+	/** The modid used to identify playerex. */
+	public static final String MODID = "playerex";
+	
+	public PlayerEx() {}
+}
