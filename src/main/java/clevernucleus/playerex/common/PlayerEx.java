7fc15bd24e57ba433e3ba09a4f8c5c16dfc65e92
CleverNucleus <jamie@mclean007.plus.com>
2020-10-04 14:12:26 +0100

    1.16.3 Progression
    -This will largely be a rewrite (but not a rework)
    -Compatibility should remain for 1.15.2 worlds

--- src/main/java/clevernucleus/playerex/common/PlayerEx.java
+++ src/main/java/clevernucleus/playerex/common/PlayerEx.java
@@ -1,11 +1,6 @@
 package clevernucleus.playerex.common;
 
-import clevernucleus.playerex.common.util.ConfigSetting;
-import clevernucleus.playerex.common.util.IProxy;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 /**
  * Mod init class; holds the modid.
@@ -16,11 +11,5 @@ public class PlayerEx {
 	/** The modid used to identify playerex. */
 	public static final String MODID = "playerex";
 	
-	/** Proxy instance to get side specific methods. */
-	public static final IProxy PROXY = DistExecutor.runForDist(() -> clevernucleus.playerex.client.ClientProxy::new, () -> clevernucleus.playerex.server.ServerProxy::new);
-	
-	public PlayerEx() {
-		ModLoadingContext.get().registerConfig(Type.COMMON, ConfigSetting.COMMON_SPEC);
-		ModLoadingContext.get().registerConfig(Type.CLIENT, ConfigSetting.CLIENT_SPEC);
-	}
+	public PlayerEx() {}
 }
