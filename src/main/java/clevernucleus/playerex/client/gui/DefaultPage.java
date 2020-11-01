2a1499ced31a57c94d1ca745dc6e222db5a9d001
CleverNucleus <jamie@mclean007.plus.com>
2020-11-01 15:43:31 +0000

    More gui stuff
    +Reimplemented all the gui elements in the previous versions
    *Lowered the text and buttons a little bit for horizontal symmetry.
    *Shortened 'Knockback Resist.' to 'Knockback Res.' so it better fits the screen.

--- src/main/java/clevernucleus/playerex/client/gui/DefaultPage.java
+++ src/main/java/clevernucleus/playerex/client/gui/DefaultPage.java
@@ -19,6 +19,7 @@ import clevernucleus.playerex.common.network.AddPlayerElement;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -32,7 +33,7 @@ public class DefaultPage extends Page {
 	private final List<DynamicTextComponent> dynamicTextComponents = new ArrayList<DynamicTextComponent>();
 	private final DecimalFormat text = new DecimalFormat("##.##");
 	
-	private final DynamicTextComponent level = new DynamicTextComponent(20, 40, (par0, par1) -> {
+	private final DynamicTextComponent level = new DynamicTextComponent(20, 50, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.level", (int)ElementRegistry.LEVEL.get(par0, par1), (int)(100 * Util.expCoeff((float)ElementRegistry.LEVEL.get(par0, par1), (float)ElementRegistry.EXPERIENCE.get(par0, par1))), "%");
 		
 		return var0.getString();
@@ -47,7 +48,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent skillPoints = new DynamicTextComponent(20, 52, (par0, par1) -> {
+	private final DynamicTextComponent skillPoints = new DynamicTextComponent(20, 62, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.skillpoints", (int)ElementRegistry.SKILLPOINTS.get(par0, par1));
 		
 		return var0.getString();
@@ -62,7 +63,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent constitution = new DynamicTextComponent(30, 76, (par0, par1) -> {
+	private final DynamicTextComponent constitution = new DynamicTextComponent(30, 86, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.constitution", (int)ElementRegistry.CONSTITUTION.get(par0, par1));
 		
 		return var0.getString();
@@ -81,7 +82,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent strength = new DynamicTextComponent(30, 100, (par0, par1) -> {
+	private final DynamicTextComponent strength = new DynamicTextComponent(30, 110, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.strength", (int)ElementRegistry.STRENGTH.get(par0, par1));
 		
 		return var0.getString();
@@ -101,7 +102,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent dexterity = new DynamicTextComponent(30, 124, (par0, par1) -> {
+	private final DynamicTextComponent dexterity = new DynamicTextComponent(30, 134, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.dexterity", (int)ElementRegistry.DEXTERITY.get(par0, par1));
 		
 		return var0.getString();
@@ -122,7 +123,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent intelligence = new DynamicTextComponent(30, 148, (par0, par1) -> {
+	private final DynamicTextComponent intelligence = new DynamicTextComponent(30, 158, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.intelligence", (int)ElementRegistry.INTELLIGENCE.get(par0, par1));
 		
 		return var0.getString();
@@ -143,7 +144,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent luckiness = new DynamicTextComponent(30, 172, (par0, par1) -> {
+	private final DynamicTextComponent luckiness = new DynamicTextComponent(30, 182, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.luckiness", (int)ElementRegistry.LUCKINESS.get(par0, par1));
 		
 		return var0.getString();
@@ -162,7 +163,7 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
-	private final DynamicTextComponent health = new DynamicTextComponent(140, 40, (par0, par1) -> {
+	private final DynamicTextComponent health = new DynamicTextComponent(130, 50, (par0, par1) -> {
 		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.health", text.format((par0.getHealth() + par0.getAbsorptionAmount())), text.format(par0.getMaxHealth()));
 		
 		return var0.getString();
@@ -180,6 +181,140 @@ public class DefaultPage extends Page {
 		
 		return var0;
 	});
+	private final DynamicTextComponent armor = new DynamicTextComponent(130, 62, (par0, par1) -> {
+		String var0 = text.format(par0.getAttribute(Attributes.ARMOR).getValue()) + " (+" + text.format(par0.getAttribute(Attributes.ARMOR).getValue() - par0.getAttribute(Attributes.ARMOR).getBaseValue()) + ")";
+		TranslationTextComponent var1 = new TranslationTextComponent("playerex.attribute.armor", var0);
+		
+		return var1.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		String var1 = text.format(par0.getAttribute(Attributes.ARMOR_TOUGHNESS).getValue()) + " (+" + text.format(par0.getAttribute(Attributes.ARMOR_TOUGHNESS).getValue() - par0.getAttribute(Attributes.ARMOR_TOUGHNESS).getBaseValue()) + ")";
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.armor.alt0")));
+		var0.add(new StringTextComponent(" "));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.armor.alt1") + var1));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.ARMOR.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent resistance = new DynamicTextComponent(130, 74, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.resistance", text.format(100D * ElementRegistry.KNOCKBACK_RESISTANCE.get(par0, par1)) + "%");
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt0") + text.format(100D * ElementRegistry.DAMAGE_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt1") + text.format(100D * ElementRegistry.FIRE_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt2") + text.format(100D * ElementRegistry.LAVA_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt3") + text.format(100D * ElementRegistry.EXPLOSION_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt4") + text.format(100D * ElementRegistry.FALLING_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt5") + text.format(100D * ElementRegistry.POISON_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt6") + text.format(100D * ElementRegistry.WITHER_RESISTANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.resistance.alt7") + text.format(100D * ElementRegistry.DROWNING_RESISTANCE.get(par0, par1)) + "%"));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.KNOCKBACK_RESISTANCE.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent melee = new DynamicTextComponent(130, 98, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.melee", text.format(ElementRegistry.MELEE_DAMAGE.get(par0, par1)));
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.melee.alt0") + text.format(100D + (100D * ElementRegistry.MELEE_CRIT_CHANCE.get(par0, par1))) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.melee.alt1") + text.format(100D * ElementRegistry.MELEE_CRIT_CHANCE.get(par0, par1)) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.melee.alt2") + text.format(ElementRegistry.ATTACK_SPEED.get(par0, par1))));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.MELEE_DAMAGE.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent ranged = new DynamicTextComponent(130, 110, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.ranged", text.format(ElementRegistry.RANGED_DAMAGE.get(par0, par1)));
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.ranged.alt0") + text.format(100D + (100D * ElementRegistry.RANGED_CRIT_CHANCE.get(par0, par1))) + "%"));
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.ranged.alt1") + text.format(100D * ElementRegistry.RANGED_CRIT_CHANCE.get(par0, par1)) + "%"));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.RANGED_DAMAGE.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent evasion = new DynamicTextComponent(130, 122, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.evasion", text.format(100D * ElementRegistry.EVASION_CHANCE.get(par0, par1)), "%");
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.evasion.alt0")));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.EVASION_CHANCE.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent lifesteal = new DynamicTextComponent(130, 146, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.lifesteal", text.format(100D * ElementRegistry.LIFESTEAL.get(par0, par1)), "%");
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.lifesteal.alt0")));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.LIFESTEAL.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent luck = new DynamicTextComponent(130, 158, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.luck", text.format(ElementRegistry.LUCK.get(par0, par1)));
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.luck.alt0")));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.LUCK.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
+	private final DynamicTextComponent movement = new DynamicTextComponent(130, 182, (par0, par1) -> {
+		TranslationTextComponent var0 = new TranslationTextComponent("playerex.attribute.movement", text.format(100D * ElementRegistry.MOVEMENT_SPEED_AMP.get(par0, par1)) + "%");
+		
+		return var0.getString();
+	}, (par0, par1) -> {
+		List<ITextComponent> var0 = new ArrayList<ITextComponent>();
+		
+		var0.add(new StringTextComponent(TextFormatting.GRAY + I18n.format("playerex.attribute.movement.alt0")));
+		
+		for(BiFunction<PlayerEntity, IPlayerElements, String> var : ClientReg.getTooltips(ElementRegistry.MOVEMENT_SPEED_AMP.getRegistryName())) {
+			var0.add(new StringTextComponent(var.apply(par0, par1)));
+		}
+		
+		return var0;
+	});
 	public DefaultPage(ITextComponent par0) {
 		super(par0);
 		
@@ -191,6 +326,14 @@ public class DefaultPage extends Page {
 		this.dynamicTextComponents.add(this.intelligence);
 		this.dynamicTextComponents.add(this.luckiness);
 		this.dynamicTextComponents.add(this.health);
+		this.dynamicTextComponents.add(this.armor);
+		this.dynamicTextComponents.add(this.resistance);
+		this.dynamicTextComponents.add(this.melee);
+		this.dynamicTextComponents.add(this.ranged);
+		this.dynamicTextComponents.add(this.evasion);
+		this.dynamicTextComponents.add(this.lifesteal);
+		this.dynamicTextComponents.add(this.luck);
+		this.dynamicTextComponents.add(this.movement);
 	}
 	
 	@Override
@@ -236,7 +379,7 @@ public class DefaultPage extends Page {
 		super.init(par1);
 		
 		for(int var = 0; var < this.idToElement.length; var++) {
-			this.addButton(new TexturedButton(par1, 8, 50 + (17 * var), 11, 10, 204, 0, var, (var0, var1) -> {
+			this.addButton(new TexturedButton(par1, 8, 58 + (17 * var), 11, 10, 204, 0, var, (var0, var1) -> {
 				Registry.NETWORK.sendToServer(new AddPlayerElement(Pair.of(this.idToElement[var1], 1D), Pair.of(ElementRegistry.SKILLPOINTS, -1D)));
 			}, null));
 		}
