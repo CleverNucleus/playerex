0663c7503a1e6003604a8e9e54488d304ad09338
CleverNucleus <jamie@mclean007.plus.com>
2021-05-07 20:38:45 +0100

    Cleanup for overhaul - fresh start

--- src/main/java/com/github/clevernucleus/playerex/PlayerEx.java
+++ src/main/java/com/github/clevernucleus/playerex/PlayerEx.java
@@ -1,22 +1,10 @@
 package com.github.clevernucleus.playerex;
 
 import com.github.clevernucleus.playerex.api.ExAPI;
-import com.github.clevernucleus.playerex.client.ClientConfig;
-import com.github.clevernucleus.playerex.util.CommonConfig;
-import com.github.clevernucleus.playerex.util.IProxy;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(ExAPI.MODID)
 public class PlayerEx {
 	
-	/** Proxy instance to get side specific methods. */
-	public static final IProxy PROXY = DistExecutor.safeRunForDist(() -> com.github.clevernucleus.playerex.client.ClientProxy::new, () -> com.github.clevernucleus.playerex.util.ServerProxy::new);
-	
-	public PlayerEx() {
-		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.COMMON_SPEC);
-		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CLIENT_SPEC);
-	}
+	public PlayerEx() {}
 }
