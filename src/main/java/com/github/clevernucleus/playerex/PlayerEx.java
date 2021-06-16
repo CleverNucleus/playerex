33c3dccceefd3670df98a0a284c18dd37519b021
CleverNucleus <jamie@mclean007.plus.com>
2021-06-16 16:08:45 +0100

    Built and setup mod and gradle

---
+++ src/main/java/com/github/clevernucleus/playerex/PlayerEx.java
@@ -0,0 +1,11 @@
+package com.github.clevernucleus.playerex;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class PlayerEx implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+}
